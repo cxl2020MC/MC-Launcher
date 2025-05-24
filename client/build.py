85b836b0e0672d8e67319a9f78f40ccd3e631adf
cxl2020MC <andamu2019@163.com>
2025-05-24 22:05:16 +0800

    切换pywebview后端从pyside6到qt，并更新Linux构建插件

--- client/build.py
+++ client/build.py
@@ -30,7 +30,7 @@ bulid_commend = "nuitka --onefile app.py --include-data-dir=../web/dist=dist --o
 if platform.system() == 'Darwin':
     bulid_commend += " --macos-create-app-bundle"
 elif platform.system() == 'Linux':
-    bulid_commend += " --enable-plugin=pyside6"
+    bulid_commend += " --enable-plugin=pyqt6"
 subprocess.run(bulid_commend, shell=True)
 # subprocess.run(['nuitka', '--mode=standalone', 'app.py', '--include-data-dir=../web/dist=dist'], shell=True)
 
