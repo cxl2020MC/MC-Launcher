bd33c1cb4ba590218cd18b8f0719bd495f3fe227
cxl2020MC <andamu2019@163.com>
2025-06-07 14:30:56 +0800

    调整ci python安装方式

---
+++ client/core/js_api.py
@@ -0,0 +1,20 @@
+import asyncio
+import threading
+
+
+
+class API:
+    def __init__(self):
+        self.loop = asyncio.new_event_loop()
+
+    # 包装异步函数为同步方法
+    def run_async(self, async_func):
+        return asyncio.run_coroutine_threadsafe(async_func, self.loop).result()
+
+    # 启动事件循环的线程
+    def start_loop(self):
+        threading.Thread(target=self._run_loop, daemon=True).start()
+
+    def _run_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
