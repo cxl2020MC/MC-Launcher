988ef7763d0cb37454a712ba2b679479e5e103f2
cxl2020MC <andamu2019@163.com>
2025-06-07 16:10:51 +0800

    feat(client): 实现 Minecraft 下载器并优化网络请求
    - 新增 MCDownload 类，用于下载 Minecraft 文件
    - 添加 aiohttp[speedups] 依赖，提升网络请求性能
    - 更新项目依赖并调整网络请求相关库

--- client/core/mcdownload.py
+++ client/core/mcdownload.py
@@ -1,1 +1,19 @@
-import 
+import aiofiles
+import aiohttp
+import asyncio
+
+class MCDownload:
+    def __init__(self, url, max_concurrent_tasks:  int = 128):
+        self.url = url
+        self.semaphore = asyncio.Semaphore(max_concurrent_tasks)
+    
+    async def download(self, path):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url) as response:
+                if response.status == 200:
+                    async with aiofiles.open(path, mode='wb') as f:
+                        await f.write(await response.read())
+
+
+
+
