8cafdae626fcbf5f0c8cea63814366c2249cf37b
cxl2020MC <andamu2019@163.com>
2025-06-22 11:12:11 +0800

    refactor(client): 优化目录路径获取方法
    - 使用 pathlib 模块替代 os.path 模块获取当前工作目录
    - 移除未使用的 current_dir 变量
    - 添加空行以提高代码可读性

--- client/app.py
+++ client/app.py
@@ -1,9 +1,9 @@
 # import webview
 import os
 import sys
-import pathlib
 import platform
 import asyncio
+from pathlib import Path
 
 from core.log import logger
 from core import config
@@ -12,29 +12,27 @@ from core import config
 logger.debug(f"Python版本: {platform.python_version()}")
 
 logger.debug(f"执行Python文件: {__file__}")
+
 # 获取单文件缓存文件夹
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-current_dir = os.path.dirname(__file__)
+# current_dir = os.path.dirname(__file__)
+current_dir = Path(__file__).parent.resolve()
 logger.debug(f"当前目录: {current_dir}")
 
 logger.debug(f"当前程序二进制文件: {sys.executable}")
 
 # 获取当前工作目录
-current_work_dir = os.getcwd()
+current_work_dir = Path.cwd()
 logger.debug(f"当前工作目录: {current_work_dir}")
 
 # 拼接前端目录
-web_dist_dir = pathlib.Path(current_dir).joinpath('dist').joinpath('index.html')
+web_dist_dir = current_dir / 'dist/index.html'
 logger.debug(f"前端目录: {web_dist_dir}")
-
-web_url = str(web_dist_dir)
 
 config_data = asyncio.run(config.load_config())
 
 
 if config_data.dev:
     logger.info("开发模式已启用!")
-
 
 
 if __name__ == '__main__':
